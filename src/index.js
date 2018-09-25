f699695e83b57a363f2d3f955937c4a36124e910
MihaP-Production <mikhailpodberezskyk@gmail.com>
2018-09-25 20:31:31 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  let k = equation.replace(/ /g, '').match(/[^\^]\d+/g);
+  let D = Math.pow(k[1], 2) - 4 * k[0] * k[2];
+
+  return [
+    (-k[1] - Math.round(Math.sqrt(D))) / (2 * k[0]),
+    (-k[1] + Math.round(Math.sqrt(D))) / (2 * k[0])
+  ].sort((a, b) => a - b);
 }
